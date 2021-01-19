2d313e4f57af1e0404730e7e7fe7d0fa5f740962
th7024 <th7024@gmail.com>
2021-01-19 18:46:22 +0900

    조건문 정리

---
+++ 문제/제어문/조건문/if1.py
@@ -0,0 +1,8 @@
+sum = 0
+while True:
+ data = int(input())
+ if data < 0:
+    print(sum)
+    break
+ else:
+  sum += data;
---
+++ 문제/제어문/조건문/if2.py
@@ -0,0 +1,11 @@
+year=int(input())
+if(year%4==0):
+    if(year%100==0):
+        if(year%400==0):
+            print("윤년")
+        else:
+            print("평년")
+    else:
+        print("윤년")    
+else:
+    print("평년")
