afc4d4ded5c488a47da7f820ddf6ac9e14ea26db
th7024 <th7024@gmail.com>
2021-02-25 02:48:06 +0900

    파이썬 3일차

---
+++ NoMad_Python Course/for_in.py
@@ -0,0 +1,10 @@
+days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+
+for i in days:   
+    if i is "Wed":   
+        break
+    else:   
+        print(i)
+
+for day in [1,2,3,4,5]:   
+    print(day)
---
+++ NoMad_Python Course/if_else_or.py
@@ -0,0 +1,12 @@
+def age_check(age):   
+    print(f"당신의 나이는{age}")
+    if age < 20:    
+        print("너는 불가능")
+    elif age ==20:    
+        print("성인 축하")
+    else:    
+        print("너는 가능")
+
+
+
+age_check(18)
